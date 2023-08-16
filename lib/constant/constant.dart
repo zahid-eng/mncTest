e3ac988a3bd4e16e10d08b994a838973126512bc
zahid-eng <zali68500@gmail.com>
2023-08-16 18:14:48 +0500

    homeAndlogin

---
+++ lib/constant/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Colors App
+
+const Color teal = Colors.teal;
+const Color black = Colors.black;
+const Color darkgreen = Color.fromARGB(255, 9, 99, 12);
+
+// Default Padding
+
+const double aapPadding = 30.0;
